b9f4f1945176d14151df4e420814a9790c602786
CelestialCrafter <celestialcraftermc@gmail.com>
2025-01-23 14:46:01 -0600

    project list

--- src/main.rs
+++ src/main.rs
@@ -2,24 +2,38 @@ pub mod args;
 pub mod errors;
 pub mod routes;
 
-use std::net::SocketAddr;
+use std::{net::SocketAddr, sync::Arc};
 
 use args::ARGS;
+use log::error;
 use warp::Filter;
 
 #[tokio::main]
 async fn main() {
     pretty_env_logger::init();
 
-    let root = warp::path::end().then(routes::index::page);
     let r#static = warp::path("static").and(warp::fs::dir("static"));
+    let root = warp::path::end().then(routes::index::page);
     let blog = warp::path("blog")
         .and(warp::path::param())
         .and_then(routes::blog::page);
 
+    let repositories = Arc::new(match routes::projects::repositories().await {
+        Ok(r) => r,
+        Err(err) => {
+            error!("could not fetch repositories: {}", err);
+            vec![]
+        }
+    });
+
+    let projects = warp::path("projects")
+        .map(move || repositories.clone())
+        .then(routes::projects::page);
+
     let routes = root
         .or(r#static)
         .or(blog)
+        .or(projects)
         .recover(routes::rejections::handle);
 
     let host = SocketAddr::from(([0, 0, 0, 0], ARGS.port));
