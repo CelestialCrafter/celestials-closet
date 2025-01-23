9c4a08c82006855b9cab3ca86d29416c906cd2d5
CelestialCrafter <celestialcraftermc@gmail.com>
2025-01-23 04:58:25 -0600

    htmx + base template

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,14 @@ use warp::Filter;
 async fn main() {
     pretty_env_logger::init();
 
-    let root = warp::get().then(routes::index::page);
-    let routes = root.recover(routes::rejections::handle);
+    let root = warp::path::end().then(routes::index::page);
+    let r#static = warp::path("static").and(warp::fs::dir("static"));
+    let woah = warp::path("woah").map(|| "bwahh :3");
+
+    let routes = root
+        .or(r#static)
+        .or(woah)
+        .recover(routes::rejections::handle);
 
     let host = SocketAddr::from(([0, 0, 0, 0], ARGS.port));
     warp::serve(routes).run(host).await;
