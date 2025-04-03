013ed3034d948462508c5a456dec25fd9099fa9c
CelestialCrafter <celestialcraftermc@gmail.com>
2025-04-02 20:28:24 -0500

    syntax highlighting organize build script fix up markdown test add all rose pine colors

---
+++ build/highlighting.rs
@@ -0,0 +1,129 @@
+use std::sync::LazyLock;
+
+use eyre::Result;
+use pulldown_cmark::{CodeBlockKind, CowStr, Event, Tag, TagEnd};
+use tree_sitter_highlight::{HighlightConfiguration, HighlightEvent, Highlighter};
+
+const HIGHLIGHT_NAMES: &[&str] = &[
+    "variable",
+    "variable.builtin",
+    "variable.member",
+    "variable.parameter",
+    "constant",
+    "constant.builtin",
+    "string",
+    "string.escape",
+    "string.regexp",
+    "string.special",
+    "boolean",
+    "number",
+    "type",
+    "type.builtin",
+    "attribute",
+    "property",
+    "function",
+    "function.builtin",
+    "constructor",
+    "operator",
+    "keyword",
+    "punctuation",
+    "comment",
+    "markup.bold",
+    "markup.italic",
+    "markup.strikethrough",
+    "markup.heading",
+    "markup.link",
+    "markup.raw",
+    "tag",
+];
+
+macro_rules! highlight_config {
+    ($(($global_name:ident, $lang_name:expr, $lang_fn:expr, $hl_query:expr, $inject_query:expr, $local_query: expr)),*) => {
+        $(static $global_name: LazyLock<HighlightConfiguration> = LazyLock::new(|| {
+            let mut config = HighlightConfiguration::new(
+                $lang_fn.into(),
+                $lang_name,
+                $hl_query,
+                $inject_query,
+                $local_query,
+            )
+            .expect("should be able to register highlight configuration");
+
+            config.configure(HIGHLIGHT_NAMES);
+
+            config
+        });)*
+
+        fn get_config(language: &str) -> Option<&'static HighlightConfiguration> {
+            match language {
+                $($lang_name => Some(&$global_name),)*
+                _ => None
+            }
+        }
+    };
+}
+
+highlight_config!(
+    (
+        RUST_CONFIG,
+        "rust",
+        tree_sitter_rust::LANGUAGE,
+        tree_sitter_rust::HIGHLIGHTS_QUERY,
+        tree_sitter_rust::INJECTIONS_QUERY,
+        ""
+    ),
+    (
+        JS_CONFIG,
+        "javascript",
+        tree_sitter_javascript::LANGUAGE,
+        tree_sitter_javascript::HIGHLIGHT_QUERY,
+        tree_sitter_javascript::INJECTIONS_QUERY,
+        tree_sitter_javascript::LOCALS_QUERY
+    )
+);
+
+fn highlight_code<'a>(
+    highlighter: &mut Highlighter,
+    config: &HighlightConfiguration,
+    code: &str,
+) -> Result<String> {
+    let highlights = highlighter.highlight(config, code.as_bytes(), None, |_| None)?;
+
+    Ok(highlights
+        .filter_map(|e| e.ok())
+        .fold(
+            String::with_capacity(code.len()),
+            |acc, event| match event {
+                HighlightEvent::Source { start, end } => {
+                    acc + &html_escape::encode_text(&code[start..end])
+                }
+                HighlightEvent::HighlightStart(highlight) => {
+                    acc + "<span class=\"" + HIGHLIGHT_NAMES[highlight.0] + "\"/>"
+                }
+                HighlightEvent::HighlightEnd => acc + "</span>",
+            },
+        ))
+}
+
+pub fn inject_highlights<'a>(
+    highlighter: &'a mut Highlighter,
+    iter: impl Iterator<Item = Event<'a>>,
+) -> impl Iterator<Item = Event<'a>> {
+    iter.scan(None, move |state, event| {
+        let inside = state.is_some();
+        match event {
+            Event::Start(Tag::CodeBlock(CodeBlockKind::Fenced(ref language))) => *state = get_config(language),
+            Event::Text(ref code) if inside => {
+                match highlight_code(highlighter, state.unwrap(), code) {
+                    Ok(highlighted) => return Some(Some(Event::Html(CowStr::from(highlighted)))),
+                    Err(err) => println!("cargo:warning=could not highlight {}: {}", code, err),
+                }
+            }
+            Event::End(TagEnd::CodeBlock) if inside => *state = None,
+            _ => (),
+        }
+
+        Some(Some(event))
+    })
+    .filter_map(|e| e)
+}
