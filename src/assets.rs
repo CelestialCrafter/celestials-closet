1ba1ef012214e697c8deb4bd12c98d05e10685d5
CelestialCrafter <celestialcraftermc@gmail.com>
2025-04-02 22:11:21 -0500

    add guestbook custom data format :3 clean up build pipeline add ed25519 key remove post revision do some new styling buncha other stuff

--- src/assets.rs
+++ src/assets.rs
@@ -1,14 +1,32 @@
 use warp::{
-    filters::path::Tail,
+    filters::{
+        fs::dir,
+        path::{path, Tail},
+        BoxedFilter,
+    },
     reject,
     reply::Reply,
+    Filter,
 };
 
 include!(concat!(env!("OUT_DIR"), "/assets.rs"));
 
+pub fn route() -> BoxedFilter<(impl Reply,)> {
+    path("assets")
+        .and(dir("assets"))
+        .map(|reply| {
+            warp::reply::with_header(
+                reply,
+                "Cache-Control",
+                format!("max-age={}", 60 * 60 * 24 * 7),
+            )
+        })
+        .boxed()
+}
+
 pub async fn page(tail: Tail) -> Result<impl Reply, reject::Rejection> {
     match ASSETS.get(tail.as_str()) {
-        Some(data) => Ok(data.clone()),
+        Some(data) => Ok(*data),
         None => Err(reject::not_found()),
     }
 }
