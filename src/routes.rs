6eaf1a75f3d197e985c73631356720ce3b36e5ec
CelestialCrafter <celestialcraftermc@gmail.com>
2025-05-14 10:24:09 -0500

    scrap comments

--- src/routes.rs
+++ src/routes.rs
@@ -2,7 +2,7 @@ use std::net::SocketAddr;
 
 use warp::{filters, reply::Reply, Filter};
 
-use crate::{assets, database::Database};
+use crate::{assets, DATABASE};
 
 pub mod database;
 pub mod index;
@@ -11,19 +11,19 @@ pub mod projects;
 pub mod personal;
 pub mod rejections;
 
-pub fn routes(db: &'static Database) -> filters::BoxedFilter<(impl Reply,)> {
+pub fn routes() -> filters::BoxedFilter<(impl Reply,)> {
     let count_view = warp::any()
         .and(filters::addr::remote())
         .map(move |addr: Option<SocketAddr>| {
-            if let Some(addr) = addr {
-                db.store(addr.ip(), None).unwrap();
+            if let (Some(addr), Some(db)) = (addr, DATABASE.get()) {
+                db.store(addr.ip());
             }
         })
         .untuple_one();
 
     assets::route()
         .or(count_view
-            .and(index::route(db))
+            .and(index::route())
             .or(projects::route())
             .or(posts::route()))
             .or(personal::route())
--- src/routes/index.rs
+++ src/routes/index.rs
@@ -1,68 +1,20 @@
-use std::{collections::HashMap, net::SocketAddr};
-
 use askama_warp::Template;
 use warp::{
     filters,
-    http::StatusCode,
-    reply::{with_status, Reply},
+    reply::Reply,
     Filter,
 };
 
-use crate::database::{Database, Message};
-
-pub fn route(db: &'static Database) -> filters::BoxedFilter<(impl Reply,)> {
+pub fn route() -> filters::BoxedFilter<(impl Reply,)> {
     warp::path::end()
-        .and(
-            filters::method::post()
-                .map(move || db)
-                .and(filters::body::form())
-                .and(filters::addr::remote())
-                .then(post)
-        .or(warp::any().map(move || db).then(page)),
-        )
+        .then(page)
         .boxed()
 }
 
 #[derive(Template)]
 #[template(path = "index.html")]
-struct IndexTemplate {
-    views: usize,
-    messages: Box<[Message]>,
-}
+struct IndexTemplate;
 
-async fn post(
-    db: &'static Database,
-    mut form: HashMap<String, String>,
-    addr: Option<SocketAddr>,
-) -> impl Reply {
-    let addr = match addr {
-        Some(addr) => addr,
-        None => {
-            return Err(with_status(
-                "server transport does not use socket addresses",
-                StatusCode::INTERNAL_SERVER_ERROR,
-            )
-            .into_response())
-        }
-    };
-
-    let name = form
-        .remove("name")
-        .ok_or(with_status("missing name field", StatusCode::BAD_REQUEST).into_response())?;
-    let content = form
-        .remove("content")
-        .ok_or(with_status("missing msg field", StatusCode::BAD_REQUEST).into_response())?;
-
-    db.store(addr.ip(), Some(Message { name, content }))
-        .map_err(|err| with_status(err.to_string(), StatusCode::BAD_REQUEST).into_response())?;
-
-    Ok(page(db).await)
-}
-
-async fn page(db: &Database) -> impl Reply + use<'_> {
-    let db = db.read().unwrap();
-    IndexTemplate {
-        views: db.len(),
-        messages: db.values().cloned().filter_map(|v| v).collect(),
-    }
+async fn page() -> impl Reply {
+    IndexTemplate
 }
