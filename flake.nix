a90adc0d218148e45fd9a5bc31f949bcbb4aa674
CelestialCrafter <celestialcraftermc@gmail.com>
2025-06-06 09:23:34 -0500

    flake build

--- flake.nix
+++ flake.nix
@@ -4,16 +4,43 @@
     { nixpkgs, ... }:
     let
       pkgs = nixpkgs.legacyPackages.x86_64-linux;
-    in {
+    in
+    {
+      packages = builtins.listToAttrs (
+        builtins.map
+          (system: {
+            name = system;
+            value = {
+              default = pkgs.rustPlatform.buildRustPackage {
+                pname = "celestials-closet";
+                version = "0.1.0";
+
+                src = nixpkgs.lib.cleanSource ./.;
+                cargoHash = "sha256-DySaaJ/uqMVqTkvG/tTqozjQ0xjTaBhTyzXs+ePoI50=";
+                useFetchCargoVendor = true;
+
+                meta = {
+                  description = " my personal website! ";
+                  homepage = "https://github.com/celestialCrafter/celestials-closet/";
+                  license = nixpkgs.lib.licenses.mpl20;
+                  maintainers = [ "CelestialCrafter" ];
+                };
+              };
+            };
+          })
+          [
+            "x86_64-linux"
+            "aarch64-linux"
+          ]
+      );
+
       devShells.x86_64-linux.default = pkgs.mkShell {
         packages = with pkgs; [
           rustc
           cargo
-          jujutsu
           pkg-config
           openssl
         ];
       };
     };
 }
-
