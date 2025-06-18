3d0af6886a48fedd20e9029917d698fe711eb4b3
CelestialCrafter <celestialcraftermc@gmail.com>
2025-06-18 16:37:27 -0500

    docker image build

--- flake.nix
+++ flake.nix
@@ -1,41 +1,47 @@
 {
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
   outputs =
     { nixpkgs, ... }:
-    let
-      pkgs = nixpkgs.legacyPackages.x86_64-linux;
-    in
     {
-      packages = builtins.listToAttrs (
-        builtins.map
-          (system: {
-            name = system;
-            value = {
-              default = pkgs.rustPlatform.buildRustPackage {
-                pname = "celestials-closet";
-                version = "0.1.0";
-
-                src = nixpkgs.lib.cleanSource ./.;
-                cargoHash = "sha256-DySaaJ/uqMVqTkvG/tTqozjQ0xjTaBhTyzXs+ePoI50=";
-                useFetchCargoVendor = true;
-
-                meta = {
-                  description = " my personal website! ";
-                  homepage = "https://github.com/celestialCrafter/celestials-closet/";
-                  license = nixpkgs.lib.licenses.mpl20;
-                  maintainers = [ "CelestialCrafter" ];
-                };
-              };
+      packages = nixpkgs.lib.genAttrs [ "x86_64-linux" "aarch64-linux" ] (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          program = pkgs.rustPlatform.buildRustPackage {
+            pname = "celestials-closet";
+            version = "0.1.0";
+            src = nixpkgs.lib.cleanSource ./.;
+            cargoHash = "sha256-DySaaJ/uqMVqTkvG/tTqozjQ0xjTaBhTyzXs+ePoI50=";
+            useFetchCargoVendor = true;
+            meta = {
+              description = "my personal website!";
+              homepage = "https://github.com/celestialCrafter/celestials-closet/";
+              license = pkgs.lib.licenses.mpl20;
+              maintainers = [ "CelestialCrafter" ];
             };
-          })
-          [
-            "x86_64-linux"
-            "aarch64-linux"
-          ]
+          };
+
+          container = pkgs.dockerTools.buildImage {
+            name = program.pname;
+            tag = "latest";
+
+            config = {
+              Cmd = [ "${program}/bin/celestials-closet" ];
+              ExposedPorts."80/tcp" = { };
+              Volumes."/data/" = { };
+              WorkingDir = "/data";
+            };
+          };
+        in
+        {
+          inherit container;
+          default = program;
+        }
       );
 
-      devShells.x86_64-linux.default = pkgs.mkShell {
-        packages = with pkgs; [
+      devShells.x86_64-linux.default = nixpkgs.legacyPackages.x86_64-linux.mkShell {
+        packages = with nixpkgs.legacyPackages.x86_64-linux; [
           rustc
           cargo
           pkg-config
