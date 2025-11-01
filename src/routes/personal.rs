60672f5d1dce986a77779d8df359ecb1f0635b3e
CelestialCrafter <celestialcraftermc@gmail.com>
2025-10-31 22:57:37 -0500

    modify personal page

--- src/routes/personal.rs
+++ src/routes/personal.rs
@@ -1,8 +1,8 @@
 use askama::Template;
 use warp::{
+    filters::{path::path, BoxedFilter},
+    reply::{html, Reply},
     Filter,
-    filters::{BoxedFilter, path::path},
-    reply::{Reply, html},
 };
 
 pub fn route() -> BoxedFilter<(impl Reply,)> {
@@ -18,7 +18,6 @@ struct Resource<'a> {
 #[derive(Template)]
 #[template(path = "personal.html")]
 struct PersonalTemplate<'a> {
-    games: Vec<Resource<'a>>,
     music: Vec<Resource<'a>>,
     anime_manga: Vec<Resource<'a>>,
 }
@@ -26,53 +25,41 @@ struct PersonalTemplate<'a> {
 async fn page() -> impl Reply {
     html(
         PersonalTemplate {
-            games: vec![
-                Resource {
-                    title: "NieR: Automata",
-                    image: "/assets/nier-automata.webp",
-                    url: "https://nierautomata.square-enix-games.com",
-                },
-                Resource {
-                    title: "NEEDY STREAMER OVERLOAD",
-                    image: "/assets/needy-streamer-overload.webp",
-                    url: "https://whysoserious.jp/needy/en",
-                },
-                Resource {
-                    title: "Persona 5 Royal",
-                    image: "/assets/persona-5-royal.webp",
-                    url: "https://persona.atlus.com/p5r",
-                },
-            ],
             music: vec![
                 Resource {
                     title: "Yorushika",
                     image: "/assets/yorushika.webp",
-                    url: "https://yorushika.com",
-                },
-                Resource {
-                    title: "NIKI",
-                    image: "/assets/niki.webp",
-                    url: "https://nikizefanya.com",
-                },
-                Resource {
-                    title: "beabadoobee",
-                    image: "/assets/beabadoobee.webp",
-                    url: "https://www.beabadoobee.com",
-                },
-                Resource {
-                    title: "Ado",
-                    image: "/assets/ado.webp",
-                    url: "http://universal-music.co.jp/ado",
-                },
-                Resource {
-                    title: "ZUTOMAYO",
-                    image: "/assets/zutomayo.webp",
-                    url: "https://zutomayo.net",
+                    url: "https://yorushika.com/",
                 },
                 Resource {
                     title: "INABAKUMORI",
                     image: "/assets/inabakumori.webp",
                     url: "https://www.youtube.com/@Inabakumori",
+                },
+                Resource {
+                    title: "tricot",
+                    image: "/assets/tricot.webp",
+                    url: "https://tricot-official.jp/"
+                },
+                Resource {
+                    title: "NIKI",
+                    image: "/assets/niki.webp",
+                    url: "https://nikizefanya.com/",
+                },
+                Resource {
+                    title: "beabadoobee",
+                    image: "/assets/beabadoobee.webp",
+                    url: "https://www.beabadoobee.com/",
+                },
+                Resource {
+                    title: "Wisp",
+                    image: "/assets/wisp.webp",
+                    url: "http://musicbywisp.com/"
+                },
+                Resource {
+                    title: "ZUTOMAYO",
+                    image: "/assets/zutomayo.webp",
+                    url: "https://zutomayo.net/",
                 },
                 Resource {
                     title: "Iyowa",
@@ -104,7 +91,7 @@ async fn page() -> impl Reply {
                 Resource {
                     title: "Witch Hat Atelier",
                     image: "/assets/witch-hat-atlier.webp",
-                    url: "https://tongari-anime.com/en/",
+                    url: "https://tongari-anime.com/en",
                 },
                 Resource {
                     title: "Oshi no Ko",
@@ -112,11 +99,26 @@ async fn page() -> impl Reply {
                     url: "https://ichigoproduction.com/",
                 },
                 Resource {
-                    title: "The Mimosa Confessions ",
+                    title: "Takopi's Original Sin",
+                    image: "/assets/takopi.webp",
+                    url: "https://www.tbs.co.jp/anime/takopi_project"
+                },
+                Resource {
+                    title: "The Mimosa Confessions",
                     image: "/assets/mimosa-confessions.webp",
-                    url: "https://sevenseasentertainment.com/series/the-mimosa-confessions-light-novel/",
+                    url: "https://sevenseasentertainment.com/series/the-mimosa-confessions-light-novel",
+                },
+                Resource {
+                    title: "The Summer Hikaru Died",
+                    image: "/assets/hikaru.webp",
+                    url: "https://hikanatsu-anime.com/"
+                },
+                Resource {
+                    title: "Girls' Last Tour",
+                    image: "/assets/girls-last-tour.webp",
+                    url: "https://girls-last-tour.com/"
                 }
-            ],
+            ]
         }
         .render()
         .expect("template should render"),
